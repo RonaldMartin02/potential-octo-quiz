ce0922c8a4e8e4fc00225aa171aea15b75f2dd62
Ronald Martin <ronaldmartin02@gmail.com>
2023-10-12 23:04:07 -0400

    removed alerts and added timer and endQuiz fuctionality

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,5 +1,13 @@
 var startBtn = document.querySelector("#start-btn")
-var quizDiv = document.querySelector("#quiz")
+
+var qDiv = document.querySelector("#quiz")
+var tHeader = document.querySelector("#timer")
+var timerEl=document.createElement("p")
+timerEl.setAttribute('style','');
+tHeader.setAttribute('style','display: flex; width:100%; justify-content:flex-end');
+
+var questionText = document.createElement("h1");
+
 var answerBtn1 = document.createElement("button");
 answerBtn1.setAttribute('id','a');
 var answerBtn2 = document.createElement("button");
@@ -8,14 +16,17 @@ var answerBtn3 = document.createElement("button");
 answerBtn3.setAttribute('id','c');
 var answerBtn4 = document.createElement("button");
 answerBtn4.setAttribute('id','d');
-var questionText = document.createElement("h1");
-var timeValue =  60;
+
+
+var aDiv = document.querySelector("#answer-div")
+var aHeader= document.createElement("h2");
+var timeLeft = 59;
 var qCount = 0;
 var qNumb = 0;
 var uScore = 0;
 var highScore = [];
 
-const myQuestions = [
+const myQs = [
   {
     question: "what is 1 + 1",
     answers: {
@@ -39,6 +50,94 @@ const myQuestions = [
     points: 5
   },
   {
+    question: "what is 5-2",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "b",
+    points: 5
+  },
+  {
+    question: "what is 6-4",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "a",
+    points: 5
+  },
+  {
+    question: "what is 11-7",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "d",
+    points: 5
+  },
+  {
+    question: "what is 8-3",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "d",
+    points: 5
+  },
+  {
+    question: "what is 4+1-4+3+6",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "a",
+    points: 5
+  },
+  {
+    question: "what is 4-5+4+2-3",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "a",
+    points: 5
+  },
+  {
+    question: "what is 42-37",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "d",
+    points: 5
+  },
+  {
+    question: "what is 4-2",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+    correctAnswer: "a",
+    points: 5
+  },
+  {
     question: "what is 4-1+2",
     answers: {
       a: "2",
@@ -51,56 +150,81 @@ const myQuestions = [
   }
 ];
 
-function checkAnswer(){ alert(this.id)
-if(this.id === myQuestions[qNumb].correctAnswer){
+function checkAnswer(){
+  //alert(this.id)
+if(this.id === myQs[qNumb].correctAnswer){
   rightAnswer();
 }
 else{
   wrongAnswer();
 }
 }
+
 function rightAnswer(){
-  uScore += myQuestions[qNumb].points;
-  if(qNumb<myQuestions.length){
+  uScore += myQs[qNumb].points;
+  if(qNumb<myQs.length){
     qNumb++;
     changeQuestion()
   } else {
-    alert("end of quiz")
+    endQuiz()
   }
 }
+
 function wrongAnswer(){
-alert("wrong "+ "");
-if(qNumb<myQuestions.length){
+if(qNumb<=myQs.length){
   qNumb++;
   changeQuestion()
 } else {
-  alert("end of quiz")
+  endQuiz()
 }
 }
-function timer(){}
+function timer(){
+    var timeInterval = setInterval(() => {
+      
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        endQuiz()
+      }
+    }, 1000);
+  }
+  
 function changeQuestion(){
-  questionText.textContent = myQuestions[qNumb].question;
-  answerBtn1.textContent = myQuestions[qNumb].answers["a"]
-  answerBtn2.textContent = myQuestions[qNumb].answers["b"];
-  answerBtn3.textContent = myQuestions[qNumb].answers["c"];
-  answerBtn4.textContent = myQuestions[qNumb].answers["d"];
+  questionText.textContent = myQs[qNumb].question;
+  answerBtn1.textContent = myQs[qNumb].answers["a"]
+  answerBtn2.textContent = myQs[qNumb].answers["b"];
+  answerBtn3.textContent = myQs[qNumb].answers["c"];
+  answerBtn4.textContent = myQs[qNumb].answers["d"];
 }
 function startQuiz(){
   document.getElementById("start-quiz").remove();
-  quizDiv.append(questionText);
-  questionText.textContent = myQuestions[qNumb].question;
-  quizDiv.append(answerBtn1);
-  answerBtn1.textContent = myQuestions[qNumb].answers["a"]
-  quizDiv.append(answerBtn2);
-  answerBtn2.textContent = myQuestions[qNumb].answers["b"];
-  quizDiv.append(answerBtn3);
-  answerBtn3.textContent = myQuestions[qNumb].answers["c"];
-  quizDiv.append(answerBtn4);
-  answerBtn4.textContent = myQuestions[qNumb].answers["d"];
+  tHeader.append(timerEl);
+  timerEl.textContent = "60 seconds remaining"
+  timer();
+  qDiv.append(questionText);
+  questionText.textContent = myQs[qNumb].question;
+  qDiv.append(answerBtn1);
+  answerBtn1.textContent = myQs[qNumb].answers["a"]
+  qDiv.append(answerBtn2);
+  answerBtn2.textContent = myQs[qNumb].answers["b"];
+  qDiv.append(answerBtn3);
+  answerBtn3.textContent = myQs[qNumb].answers["c"];
+  qDiv.append(answerBtn4);
+  answerBtn4.textContent = myQs[qNumb].answers["d"];
 }
-function endQuiz(){}
-function saveScore(){
-  localStorage.setItem('highScore',JSON.stringify())
+function endQuiz(){
+  document.getElementById("quiz").remove();
+  saveScore(uScore)
+}
+function saveScore(event){
+  event.preventDefault();
+  localStorage.setItem('highScore',uScore)
 }
 function getScore(){
   var storedhScore = JSON.parse(localStorage.getItem("highScore"));
@@ -109,7 +233,7 @@ function getScore(){
   }
 }
 function renderHighScore(){
-
+getScore()
 }
 
 
